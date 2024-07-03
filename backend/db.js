4045b9bf27ba59d3b003ec7536fd7470631f2188
Rahimakhan01 <khanrahima1311@gmail.com>
2024-07-03 15:34:54 +0530

    Deleting a Note

--- backend/db.js
+++ backend/db.js
@@ -7,6 +7,7 @@ const connectToMongo = async () => {
         await mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
         console.log("Connected to Mongo Successfully");
     } catch (error) {
+        
         console.error("Error connecting to Mongo", error);
     }
 };

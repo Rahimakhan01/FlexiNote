04a5020d890beb3cc6f8a0794d25edecec37f646
Rahimakhan01 <khanrahima1311@gmail.com>
2025-01-23 00:02:50 +0530

    home

--- src/App.js
+++ src/App.js
@@ -11,25 +11,27 @@ import About from './components/About';
 import Alert from './components/Alert';
 import Signup from './components/Signup';
 import Login from './components/Login';
+import AddNote from './components/AddNote'; // Import AddNote Component
 
 import NoteState from './context/notes/NoteState';
 
 function App() {
   return (
     <>
-    <NoteState>
-      <Router>
-        <Navbar /> 
-        <Alert message="This is an amazing React course" />
-        <div className="container" style={{ backgroundColor: '#ADEEE3', minHeight: '100vh' }}>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-          </Routes>
-        </div>
-      </Router>
+      <NoteState>
+        <Router>
+          <Navbar /> 
+          <Alert message="Welcome to FlexiNote, your note management solution!" />
+          <div className="container" style={{ backgroundColor: '#ADEEE3', minHeight: '100vh' }}>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/addnote" element={<AddNote />} /> {/* Add AddNote Route */}
+            </Routes>
+          </div>
+        </Router>
       </NoteState>
     </>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,52 @@
-import Notes from './Notes';
+import React from "react";
+import { Link } from "react-router-dom";
 
-const Home =(props) => {
-	const {showAlert} = props;
-	
-	return (
-		<div>
-			
-			<Notes showAlert = {showAlert}/>
-        
+const Home = (props) => {
+  return (
+    <div>
+      <div
+        style={{
+          backgroundColor: "#084C61",
+          color: "#E3B505",
+          padding: "50px",
+          textAlign: "center",
+        }}
+      >
+        <h1 style={{ fontSize: "48px", fontWeight: "bold" }}>Welcome to FlexiNote</h1>
+        <p style={{ fontSize: "20px", color: "#ADEEE3" }}>
+          Your one-stop solution to organize and manage your notes.
+        </p>
+        <div style={{ marginTop: "20px" }}>
+          <Link
+            to="/login"
+            className="btn mx-2"
+            style={{
+              backgroundColor: "#DB504A",
+              borderColor: "#DB504A",
+              color: "white",
+              padding: "10px 20px",
+              fontSize: "18px",
+            }}
+          >
+            Login
+          </Link>
+          <Link
+            to="/signup"
+            className="btn mx-2"
+            style={{
+              backgroundColor: "#DB504A",
+              borderColor: "#DB504A",
+              color: "white",
+              padding: "10px 20px",
+              fontSize: "18px",
+            }}
+          >
+            Signup
+          </Link>
         </div>
-	)
-}
-export default Home
+      </div>
+    </div>
+  );
+};
+
+export default Home;
